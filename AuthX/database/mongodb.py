18b4aaba1e2b5682daf1e84e912caeef1a1dec74
Yasser Tahiri <yasserth19@gmail.com>
2021-08-25 21:21:04 +0100

    chore : Add Database Provider and Redis as cache Provider

---
+++ AuthX/database/mongodb.py
@@ -0,0 +1,99 @@
+from typing import Iterable, Optional, Tuple
+
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCollection,
+    AsyncIOMotorDatabase,
+)
+from pymongo import ReturnDocument
+
+
+class MongoDBBackend:
+    def __init__(self, database_name: str = "test") -> None:
+        self._database_name = database_name
+
+    def set_client(self, client: AsyncIOMotorClient) -> None:
+        self._client = client
+        self.init()
+
+    def init(self) -> None:
+        self._db: AsyncIOMotorDatabase = self._client[self._database_name]
+        self._users: AsyncIOMotorCollection = self._db["users"]
+        self._email_confirmations: AsyncIOMotorCollection = self._db[
+            "email_confirmations"
+        ]
+        self._counters: AsyncIOMotorCollection = self._db["counters"]
+
+        self._settings: AsyncIOMotorCollection = self._db["settings"]
+
+    async def _increment_id(self) -> int:
+        ret = await self._counters.find_one_and_update(
+            {"name": "users"}, {"$inc": {"c": 1}}, return_document=ReturnDocument.AFTER,
+        )
+        return ret.get("c")
+
+    async def get(self, id: int) -> Optional[dict]:
+        return await self._users.find_one({"id": id}, {"_id": 0})
+
+    async def get_by_email(self, email: str) -> Optional[dict]:
+        return await self._users.find_one({"email": email}, {"_id": 0})
+
+    async def get_by_username(self, username: str) -> Optional[dict]:
+        return await self._users.find_one({"username": username}, {"_id": 0})
+
+    async def get_by_social(self, provider: str, sid: str) -> Optional[dict]:
+        return await self._users.find_one(
+            {"provider": provider, "sid": str(sid)}, {"_id": 0}
+        )
+
+    async def create(self, obj: dict) -> int:
+        async with await self._client.start_session() as session:
+            async with session.start_transaction():
+                id = await self._increment_id()
+                obj.update({"id": id})
+                await self._users.insert_one(obj)
+        return id
+
+    async def update(self, id: int, obj: dict) -> bool:
+        res = await self._users.update_one({"id": id}, {"$set": obj})
+        return bool(res.matched_count)
+
+    async def delete(self, id: int) -> bool:
+        res = await self._users.delete_one({"id": id})
+        return bool(res.deleted_count)
+
+    async def count(self, query: Optional[dict] = None) -> int:
+        return await self._users.count_documents(query)
+
+    async def request_email_confirmation(self, email: str, token_hash: str) -> None:
+        await self._email_confirmations.update_one(
+            {"email": email}, {"$set": {"token": token_hash}}, upsert=True
+        )
+        return None
+
+    async def confirm_email(self, token_hash: str) -> bool:
+        ec = await self._email_confirmations.find_one({"token": token_hash})
+        if ec is not None:
+            email = ec.get("email")
+            async with await self._client.start_session() as session:
+                async with session.start_transaction():
+                    await self._users.update_one(
+                        {"email": email}, {"$set": {"confirmed": True}}
+                    )
+                    await self._email_confirmations.delete_many({"email": email})
+            return True
+        else:
+            return False
+
+    async def get_blacklist(self) -> Iterable[dict]:
+        return await self._users.find({"active": False}, {"_id": 0}).to_list(None)
+
+    async def search(self, f: dict, p: int, size: int) -> Tuple[dict, int]:
+        count = await self._users.count_documents(f)
+        items = (
+            await self._users.find(f, {"_id": 0})
+            .skip((p - 1) * size)
+            .limit(size)
+            .to_list(None)
+        )
+        return items, count
---
+++ AuthX/database/redis.py
@@ -0,0 +1,41 @@
+from typing import Iterable, Optional, Union
+
+from aioredis import Redis
+
+
+class RedisBackend:
+    _redis: Optional[Redis] = None
+
+    def set_client(self, redis: Redis) -> None:
+        self._redis = redis
+
+    async def get(self, key: str) -> str:
+        return await self._redis.get(key)
+
+    async def delete(self, key: str) -> None:
+        await self._redis.delete(key)
+        return None
+
+    async def keys(self, match: str) -> Iterable[str]:
+        return await self._redis.keys(match)
+
+    async def set(
+        self, key: str, value: Union[str, bytes, int], expire: int = 0
+    ) -> None:
+        await self._redis.set(key, value, expire=expire)
+        return None
+
+    async def setnx(self, key: str, value: Union[str, bytes, int], expire: int) -> None:
+        await self._redis.setnx(key, value)
+        await self._redis.expire(key, expire)
+        return None
+
+    async def incr(self, key: str) -> str:
+        return await self._redis.incr(key)
+
+    # async def incrby(self, key: str, i: int) -> str:
+    #     return await self._redis.incrby(key, i)
+
+    async def dispatch_action(self, channel: str, action: str, payload: dict) -> None:
+        await self._redis.publish_json(channel, {"action": action, "payload": payload})
+        return None
