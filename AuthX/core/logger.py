56a7d2df68a915f3ee80889e0e3b0a5ae076a1a1
Yasser Tahiri <yasserth19@gmail.com>
2021-08-25 21:16:14 +0100

    chore: Create Package Core

---
+++ AuthX/core/logger.py
@@ -0,0 +1,31 @@
+import logging
+import logging.handlers
+
+from AuthX.core.config import DEBUG
+
+
+class LevelFilter:
+    def __init__(self, level):
+        self._level = level
+
+    def filter(self, log_record):
+        return log_record.levelno == self._level
+
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s %(message)s")
+
+if DEBUG:
+    log_info = logging.StreamHandler()
+else:
+    log_info = logging.handlers.RotatingFileHandler(
+        "/var/log/web/users/info.log", maxBytes=1_000_000 * 10, backupCount=15
+    )
+
+log_info.addFilter(LevelFilter(logging.INFO))  # type: ignore
+
+log_info.setFormatter(formatter)
+
+logger.addHandler(log_info)
---
+++ AuthX/core/password.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(
+    schemes=["bcrypt", "django_pbkdf2_sha256"], deprecated="auto"
+)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
