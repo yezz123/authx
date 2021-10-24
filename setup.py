61d02dded7c23b078bced2641d045a5078eaa399
Yasser Tahiri <yasserth19@gmail.com>
2021-10-24 02:06:11 +0100

    chore: Provide all requires relate to Setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+import os
+import typing as t
+
 import setuptools
 
-__version__ = "0.0.9"
+from AuthX import __author__, __author_email__, __license__, __version__
+
+directory = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+def parse_requirement(req_path: str) -> t.List[str]:
+    with open(os.path.join(directory, "", req_path)) as f:
+        contents = f.read()
+        return [i.strip() for i in contents.strip().split("\n")]
+
+
 setuptools.setup(
     name="AuthX",
     version=__version__,
-    author="Yasser Tahiri",
-    author_email="yasserth19@gmail.com",
+    author=__author__,
+    platforms=["any"],
     description="Ready to use and customizable Authentications and Oauth2 management for FastAPI",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,36 +31,29 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/yezz123/AuthX/issues",
         "Funding": "https://opencollective.com/yezz123",
     },
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
+    license=__license__,
     include_package_data=True,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Customer Service",
         "Development Status :: 4 - Beta",
         "Natural Language :: English",
+        "Framework :: aiohttp",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Internet :: WWW/HTTP :: Session",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
         "Topic :: Utilities",
         "Typing :: Typed",
     ],
-    python_requires=">=3.7",
-    install_requires=[
-        "fastapi==0.70.0",
-        "PyJWT==1.7.1",
-        "cryptography==35.0.0",
-        "httpx==0.20.0",
-        "aioredis==1.3.1",
-        "passlib==1.7.4",
-        "itsdangerous==1.1.0",
-        "bcrypt==3.2.0",
-        "email-validator==1.1.1",
-        "motor==2.3.0",
-        "aiosmtplib==1.1.4",
-        "pydantic==1.8.2",
-    ],
+    python_requires=">=3.8",
+    install_requires=parse_requirement("requirements.txt"),
     extras_require={"dev": ["requests == 2.25.1", "uvicorn"]},
 )
