ff3af2ed5d4eedf0c401b713d98b084452b5a015
Yasser Tahiri <yasserth19@gmail.com>
2021-11-25 01:16:54 +0100

    chore: Add Py3.7 to Classifiers ✨

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setuptools.setup(
         "Natural Language :: English",
         "Framework :: FastAPI",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
