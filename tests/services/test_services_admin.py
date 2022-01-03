d82deff3acd72efa2faafa53b18a5578cd7c0a1e
Yasser Tahiri <yasserth19@gmail.com>
2022-01-03 02:30:00 +0100

    Fix AttributeError issue

--- tests/services/test_services_admin.py
+++ tests/services/test_services_admin.py
@@ -28,33 +28,33 @@ def admin_service_setup():
 
 
 @pytest.mark.asyncio
-async def test_get_blacklist(admin_service_setup):
+async def test_get_blacklist():
     blacklist = await admin_service_setup.get_blacklist()
     assert blacklist == []
 
 
 @pytest.mark.asyncio
-async def test_toggle_blacklist(admin_service_setup):
+async def test_toggle_blacklist():
     await admin_service_setup.toggle_blacklist(1)
     blacklist = await admin_service_setup.get_blacklist()
     assert blacklist == [1]
 
 
 @pytest.mark.asyncio
-async def test_get_blackout(admin_service_setup):
+async def test_get_blackout():
     ts = await admin_service_setup.get_blackout()
     assert ts is None
 
 
 @pytest.mark.asyncio
-async def test_set_blackout(admin_service_setup):
+async def test_set_blackout():
     await admin_service_setup.set_blackout(1)
     ts = await admin_service_setup.get_blackout()
     assert ts == 1
 
 
 @pytest.mark.asyncio
-async def test_delete_blackout(admin_service_setup):
+async def test_delete_blackout():
     await admin_service_setup.set_blackout(1)
     await admin_service_setup.delete_blackout()
     ts = await admin_service_setup.get_blackout()
@@ -62,19 +62,19 @@ async def test_delete_blackout(admin_service_setup):
 
 
 @pytest.mark.asyncio
-async def get_id_by_username(admin_service_setup):
+async def get_id_by_username():
     id = await admin_service_setup.get_id_by_username("admin")
     assert id == 1
 
 
 @pytest.mark.asyncio
-async def get_permissions_by_username(admin_service_setup):
+async def get_permissions_by_username():
     permissions = await admin_service_setup.get_permissions_by_username("admin")
     assert permissions == ["admin"]
 
 
 @pytest.mark.asyncio
-async def update_permissions(admin_service_setup):
+async def update_permissions():
     await admin_service_setup.update_permissions("admin", ["admin", "user"])
     permissions = await admin_service_setup.get_permissions_by_username("admin")
     assert permissions == ["admin", "user"]
