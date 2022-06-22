013ef54ed0fd8ec7121cbb6f416dd8f548a0e388
Yasser Tahiri <yasserth19@gmail.com>
2022-06-22 02:33:29 +0100

    👷 fix failed tests in Oauth2 (#241)

--- authx/middleware/Oauth2.py
+++ authx/middleware/Oauth2.py
@@ -31,8 +31,8 @@ class MiddlewareOauth2:
         self.db = db
         self._force_https_redirect = force_https_redirect
 
-        self._client = starlette_client.OAuth(
-            starlette_client.StarletteRemoteApp("starlette"),
+        self._client = starlette_client.StarletteRemoteApp(
+            starlette_client.StartletteIntegration("starlette"),
             client_id=client_id,
             client_secret=client_secret,
             server_metadata_url=server_metadata_url,
---
+++ tests/test_version.py
@@ -0,0 +1,5 @@
+import authx
+
+
+def test_version():
+    assert authx.__version__ == "0.5.1"
