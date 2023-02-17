7854434c362baa8590eb934deded37457c80e7be
Yasser Tahiri <yasserth19@gmail.com>
2023-02-17 20:30:54 +0400

    🔧 Add `docker.sh` to test locally

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,6 +6,9 @@ rm -f `find . -type f -name '.*~' `
 rm -f `find . -type f -name .coverage`
 rm -f `find . -type f -name ".coverage.*"`
 rm -rf `find . -name __pycache__`
+rm -rf `find . -name authx_profiling_results.html`
+rm -rf `find . -name authx_profiling_results.json`
+rm -rf `find . -name users.db`
 rm -rf `find . -type d -name '*.egg-info' `
 rm -rf `find . -type d -name 'pip-wheel-metadata' `
 rm -rf `find . -type d -name .pytest_cache`
