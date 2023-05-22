e8a8e4306bf6f2587e07588cc9cf4f8d3e4c4f3c
Yasser Tahiri <yasserth19@gmail.com>
2023-05-22 16:18:03 +0400

    ⚰️ remove dead code

--- authx/backend/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Get The API Repositories"""
-
-from authx.backend.api import UsersRepo
-
-__all__ = ["UsersRepo"]
--- authx/cache/__init__.py
+++
@@ -1,4 +0,0 @@
-from authx.cache.config import basicConfig
-from authx.cache.redis import RedisBackend
-
-__all__ = ["RedisBackend", "basicConfig"]
