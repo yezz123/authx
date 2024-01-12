895980d0bcb3f92258665d724fdc548d5db7f7a4
Yasser Tahiri <yasserth19@gmail.com>
2024-01-12 01:17:04 +0100

    ⬆️ Update dependencies in requirements files

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -6,7 +6,6 @@ refresh-lockfiles() {
   pip-compile -q --resolver backtracking -o requirements/linting.txt requirements/linting.in
   pip-compile -q --resolver backtracking -o requirements/testing.txt requirements/testing.in
   pip-compile -q --resolver backtracking -o requirements/docs.txt requirements/docs.in
-  pip-compile -q --resolver backtracking -o requirements/optional.txt requirements/optional.in
   pip-compile -q --resolver backtracking --extra all -o requirements/pyproject.txt pyproject.toml
   pip install --dry-run -r requirements/all.txt
 }
