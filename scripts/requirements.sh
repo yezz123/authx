759be12b391c2c574daa4badee43dff22e8a7eeb
Yasser Tahiri <yasserth19@gmail.com>
2024-02-26 16:00:47 +0100

    🔨 Update script to use `uv` instead of `pip-compile`

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 refresh-lockfiles() {
-  echo "Updating requirements/*.txt files using pip-compile"
+  echo "Updating requirements/*.txt files using uv"
   find requirements/ -name '*.txt' ! -name 'all.txt' -type f -delete
-  pip-compile -q --resolver backtracking -o requirements/linting.txt requirements/linting.in
-  pip-compile -q --resolver backtracking -o requirements/testing.txt requirements/testing.in
-  pip-compile -q --resolver backtracking -o requirements/docs.txt requirements/docs.in
-  pip-compile -q --resolver backtracking --extra all -o requirements/pyproject.txt pyproject.toml
-  pip install --dry-run -r requirements/all.txt
+  uv pip compile requirements/linting.in -o requirements/linting.txt
+  uv pip compile requirements/testing.in -o requirements/testing.txt
+  uv pip compile requirements/docs.in -o requirements/docs.txt
+  uv pip compile pyproject.toml -o requirements/pyproject.txt
+  uv pip install -r requirements/all.txt
 }
 
 refresh-lockfiles
