f15637dc408e8abf295191966b65f9766cb0b950
Yasser Tahiri <yasserth19@gmail.com>
2024-05-01 18:56:49 +0100

    🔧 Remove usage of Uv for extra tests

--- scripts/test_extra.sh
+++ scripts/test_extra.sh
@@ -5,9 +5,9 @@ set -e
 
 pushd "$(dirname $0)/../authx-extra"
 
-uv pip install -r requirements/all.txt
+pip install -r requirements/all.txt
 
-uv pip install -e ../
+pip install -e ../
 
 pytest --cov=authx_extra --cov-report=xml
 
