ebf7489b22abe80fc7bc2d9df832eff056b1c6cc
Maxim Krivodaev <maxim.krivodaev@gmail.com>
2025-03-15 11:41:27 +0300

    Deprecate data keyword argument in decode_token
    It just used to pass this parameter to jwt.decode, but it deprecated.
    https://github.com/jpadilla/pyjwt/blob/29fbfc/jwt/api_jwt.py#L343-L350

--- authx/token.py
+++ authx/token.py
@@ -3,6 +3,7 @@
 import datetime
 from collections.abc import Sequence
 from typing import Any, Optional, Union
+import warnings
 
 import jwt
 
@@ -103,6 +104,14 @@ def decode_token(
     data: Optional[dict[str, Any]] = None,
 ) -> dict[str, Any]:
     """Decode a token."""
+    if data is not None:
+        warnings.warn(
+            "passing data keyword argument to decode_token() is deprecated "
+            "and will be removed in authx version 2.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+
     # Default to HS256 if no algorithms are provided
     if algorithms is None:  # pragma: no cover
         algorithms = ["HS256"]  # pragma: no cover
@@ -117,7 +126,6 @@ def decode_token(
             audience=audience,
             issuer=issuer,
             options={"verify_signature": verify},
-            data=data,
         )
     except Exception as e:
         raise JWTDecodeError(*e.args) from e
